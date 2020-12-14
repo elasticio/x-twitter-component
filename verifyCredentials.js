0be643ff0b0478c3374364d497b7c95a89e54017
Olha Virolainen <olga.shukel@gmail.com>
2020-12-14 13:49:50 +0200

    Update Sailor version to 2.6.21, enable circleci and eslint

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,38 +1,37 @@
+/* eslint-disable camelcase,consistent-return */
 const Twitter = require('twitter');
 
-module.exports = verify;
+module.exports = function verify(credentials, callback) {
+  (async () => {
+    const {
+      consumer_key,
+      consumer_secret,
+      access_token_key,
+      access_token_secret,
+    } = credentials;
 
-function verify(credentials, callback) {
-    (async () => {
-        const {
-            consumer_key,
-            consumer_secret,
-            access_token_key,
-            access_token_secret
-        } = credentials;
+    const client = new Twitter({
+      consumer_key,
+      consumer_secret,
+      access_token_key,
+      access_token_secret,
+    });
 
-        const client = new Twitter({
-            consumer_key,
-            consumer_secret,
-            access_token_key,
-            access_token_secret
-        });
+    try {
+      const tweets = await client.get('search/tweets', { q: 'hi', count: 1 });
 
-        try {
-            const tweets = await client.get('search/tweets', {q: 'hi', count: 1});
+      if (tweets.statuses) {
+        return callback(null, true);
+      }
+    } catch (e) {
+      return callback(e);
+    }
 
-            if (tweets.statuses) {
-                return callback(null, true);
-            }
-        } catch (e) {
-            return callback(e);
-        }
-
-        callback({
-            message: 'Something went wrong'
-        });
-    })();
-}
+    callback({
+      message: 'Something went wrong',
+    });
+  })();
+};
 
 // verify({
 // }, (err, data) => {
